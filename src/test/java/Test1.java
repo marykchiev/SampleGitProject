e0e8daae0ec947b2a275d805aa01ef103a7ed9cf
marykchiev <51775615+marykchiev@users.noreply.github.com>
2019-08-02 19:23:24 -0500

    Update Test1.java

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -2,5 +2,11 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello Marat");
         System.out.println("Hello Maks");
+        System.out.println("Hello Maks");
+        System.out.println("Hello Maks");
+        System.out.println("Hello Maks");
+        System.out.println("Hello Maks");
+        System.out.println("Hello Maks");
+        System.out.println("Hello Maks");
     }
 }
